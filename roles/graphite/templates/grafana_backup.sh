50c154afa87402f8ddbc2d79f686ab74f6f8142d
Christian Schnidrig <christian.schnidrig@switch.ch>
2017-03-29 16:51:30 +0200

    graphite: make redundant cluster

---
+++ roles/graphite/templates/grafana_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+S3CMD="/usr/bin/s3cmd --access_key={{rgw_backup.access_key}} --secret_key={{rgw_backup.secret_key}}  --host-bucket={{rgw_backup.host_bucket}} --host={{rgw_backup.host_base}} "
+BACKUP_DIR_BASE="/mnt/data/grafana/backup"
+BACKUP_DIR="${BACKUP_DIR_BASE}/{{ansible_hostname}}"
+DAYS_TO_KEEP=7
+
+/usr/bin/docker stop grafana
+/bin/mkdir -p "${BACKUP_DIR}"
+/bin/cp /mnt/data/grafana/data/grafana.db "${BACKUP_DIR}"/grafana.db-`date "+%Y%m%d"`
+/usr/bin/find "${BACKUP_DIR}" -mtime +${DAYS_TO_KEEP} -exec /bin/rm {} \;
+
+$S3CMD sync --delete-removed "${BACKUP_DIR}/" "s3://{{rgw_backup.bucket_name}}/{{ansible_hostname}}/"
+
+/usr/bin/docker start grafana
--- roles/graphite_image/files/graphite/run.sh
+++ roles/graphite_image/files/graphite/run.sh
@@ -4,11 +4,13 @@ start() {
     echo "Starting server..."
     sudo -u _graphite /usr/bin/python /usr/lib/python2.7/dist-packages/graphite/manage.py syncdb --noinput
     service carbon-cache start
-	/usr/sbin/apache2ctl start
+    /usr/bin/carbon-relay --config=/etc/carbon/carbon.conf --pidfile=/var/run/carbon-relay.pid --logdir=/var/log/carbon/ start
+	  /usr/sbin/apache2ctl start
 }
 
 stop() {
     echo "Stopping server..."
+    /usr/bin/carbon-relay --config=/etc/carbon/carbon.conf --pidfile=/var/run/carbon-relay.pid --logdir=/var/log/carbon/ stop
     service carbon-cache stop
     /usr/sbin/apache2ctl stop
     exit
