8496821f489ecd945fae43177f344fb2b5c75f3b
Christian Schnidrig <christian.schnidrig@switch.ch>
2017-04-10 23:02:35 +0200

    fix s3_backup: move to grafana

--- roles/grafana/templates/grafana_backup.sh
+++ roles/grafana/templates/grafana_backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 S3CMD="/usr/bin/s3cmd --access_key={{rgw_backup.access_key}} --secret_key={{rgw_backup.secret_key}}  --host-bucket={{rgw_backup.host_bucket}} --host={{rgw_backup.host_base}} "
-BACKUP_DIR_BASE="/mnt/data/grafana/backup"
+BACKUP_DIR_BASE="{{grafana_data_dir}}/backup"
 BACKUP_DIR="${BACKUP_DIR_BASE}/{{ansible_hostname}}"
 DAYS_TO_KEEP=7
 
 /usr/bin/docker stop grafana
 /bin/mkdir -p "${BACKUP_DIR}"
-/bin/cp /mnt/data/grafana/data/grafana.db "${BACKUP_DIR}"/grafana.db-`date "+%Y%m%d"`
+/bin/cp "{{grafana_data_dir}}/data/grafana.db" "${BACKUP_DIR}"/grafana.db-`date "+%Y%m%d"`
 /usr/bin/find "${BACKUP_DIR}" -mtime +${DAYS_TO_KEEP} -exec /bin/rm {} \;
 
 $S3CMD sync --delete-removed "${BACKUP_DIR}/" "s3://{{rgw_backup.bucket_name}}/{{ansible_hostname}}/"
