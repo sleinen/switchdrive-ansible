8a32903285ae657e2c589cbc0795ee241ae8b99f
Fergus Kerins <fergus.kerins@switch.ch>
2018-02-21 17:12:23 +0100

    Tools for Patching

---
+++ jobs/scripts/block_web_prod.sh
@@ -0,0 +1,2 @@
+openstack server remove security group e60d8afa-2a8b-4e3c-ae2d-22111786d03f 6cb6a5f2-7ecf-462f-8493-cb5a523f6419
+openstack server remove security group a5ffd219-1444-45c4-b995-e429544833c9 6cb6a5f2-7ecf-462f-8493-cb5a523f6419
---
+++ jobs/scripts/enable_web_stage.sh
@@ -0,0 +1,2 @@
+openstack server add security group 2645c44f-7f1e-4d60-af0c-4fb1a9fb54a2 41bc1565-4303-458e-8771-daf7a88f754a
+openstack server add security group f32d22f5-c0e9-498e-815b-04bea5f2b6f8 41bc1565-4303-458e-8771-daf7a88f754a
