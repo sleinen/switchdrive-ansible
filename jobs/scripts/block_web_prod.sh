dee32354b1dc27d041c185840b473f63d3854d48
Fergus Kerins <fergus.kerins@switch.ch>
2018-02-23 10:22:24 +0100

    Adding Internal security Group

--- jobs/scripts/block_web_prod.sh
+++ jobs/scripts/block_web_prod.sh
@@ -1,2 +1,5 @@
 openstack server remove security group e60d8afa-2a8b-4e3c-ae2d-22111786d03f 6cb6a5f2-7ecf-462f-8493-cb5a523f6419
 openstack server remove security group a5ffd219-1444-45c4-b995-e429544833c9 6cb6a5f2-7ecf-462f-8493-cb5a523f6419
+# Now we enable access for SWITCH IPS
+openstack server add security group e60d8afa-2a8b-4e3c-ae2d-22111786d03f a3bc3bf1-129a-4bea-adaf-eb915676192e
+openstack server add security group a5ffd219-1444-45c4-b995-e429544833c9 a3bc3bf1-129a-4bea-adaf-eb915676192e
